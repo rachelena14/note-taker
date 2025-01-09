aed2618f711512d9d81c3b2e929973ac001120a9
rlaquea <rlaquea14@gmail.com>
2025-01-08 21:55:45 -0500

    added webpack, fixed build script

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Entry point for your application
+  output: {
+    filename: 'bundle.js', // Output file name
+    path: path.resolve(__dirname, 'public/assets/js'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply this rule to .js files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader', // Use Babel loader for transpiling JavaScript
+        },
+      },
+      {
+        test: /\.css$/, // Apply this rule to .css files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader for CSS files
+      },
+    ],
+  },
+};

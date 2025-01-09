b03cc853c88db5bcdabf6517e049ef0aeed3b97c
rlaquea <rlaquea14@gmail.com>
2025-01-08 19:53:41 -0500

    added files for deployment

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 //ensures express and the routes are being utilized
+
+require('dotenv').config();
 const express = require('express'); 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-//designates which routes the data will follow 
+
+//Designates which routes the data will follow 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Oops! I think something went wrong!');
+  });
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 

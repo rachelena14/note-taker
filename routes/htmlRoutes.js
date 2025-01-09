a1ad7366ff8bd45fb78c14387d7cdbcc618183eb
rlaquea <rlaquea14@gmail.com>
2025-01-09 18:29:56 -0500

    added path to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 //when localhost is accessed, will render notes.html file
-router.get('/notes.html', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
 //when localhost is accessed, will render index.html file

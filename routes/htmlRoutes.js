92835bf17a97ecb8bde2f2fe496a51bd1c88d391
rlaquea <rlaquea14@gmail.com>
2025-01-09 19:21:26 -0500

    troubleshooting 404

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,23 @@
 const path = require('path');
 const router = require('express').Router();
 
-//When /notes is accessed, this will render notes.html file
+// When /notes is accessed, this will render notes.html file
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-})
-//when localhost is accessed, will render index.html file
+    res.sendFile(path.join(__dirname, '../public/notes.html'), (err) => {
+        if (err) {
+            res.status(500).send('Error loading notes.html');
+        }
+    });
+});
+
+// When any other route is accessed, will render index.html file
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-})
+    res.sendFile(path.join(__dirname, '../public/index.html'), (err) => {
+        if (err) {
+            res.status(500).send('Error loading index.html');
+        }
+    });
+});
 
 module.exports = router;
 
